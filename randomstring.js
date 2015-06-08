c90ef3863f82ba6cf9e2abdc99f2ee0214c2915a
Dewey Luu <deweyluu@gmail.com>
2015-06-08 10:42:24 -0700

    random string for quiz

---
+++ randomstring.js
@@ -0,0 +1,29 @@
+
+var randomness = ["hi", "what", "is", "going", "on"];
+
+var replace = $("#hello");
+
+//replace.text(randomness);
+
+function random(x) {
+	var temp = x.length;
+	var temp2;
+	var temp3;
+	if (temp > 0) {
+		temp2 = Math.floor(Math.random() * temp);
+		temp--;
+		temp3 = x[temp];
+		x[temp] = x[temp2];
+		x[temp2] = temp3;
+	}
+	console.log(x);
+	return x;
+};
+
+$("button").on("click", function() {
+	var temp = random(randomness);
+	replace.text(temp);
+});
+
+//var temp2 = randomness.length;
+//var temp = Math.random() * temp2;
